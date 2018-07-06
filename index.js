cc553b8fc16676d08a095409adf5143d6d104250
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-06 14:25:33 +0300

    add login test

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ const getDriver = () => {
     return driver;
 }
 const browser = getDriver();
+
+const login = (name, password) => {
+    browser.findElement(webdriver.By.css('#login_field')).sendKeys(name)
+        .then(() => browser.findElement(webdriver.By.css('#password')).sendKeys(password))
+        .then(() => browser.findElement(webdriver.By.css('input[name="commit"]')).click());
+}
 browser.get('https://github.com')
-    .then(() => browser.sleep(5000))
-    .then(() => browser.quit())
+    .then(() => {
+        browser.findElement(webdriver.By.css('a[href="/login"]')).click()
+            .then(() => login('UserForTests', 'GitTest_1'));
+    })
+    .then(() => {
+        browser.findElement(webdriver.By.css('summary[class="HeaderNavlink name mt-1"]')).click()
+        .then(()=>{
+            browser.findElement(webdriver.By.css('strong.css-truncate-target')).getText()
+            .then((result)=>console.log(result));
+        });
+        browser.quit();
+    })
     .catch((err) => { console.log(err) });
