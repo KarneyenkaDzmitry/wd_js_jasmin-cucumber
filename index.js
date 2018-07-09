5400e9bfd250162e2ae4fcca230c45c57383b1d9
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-07-09 13:08:27 +0300

    play around with different options webDriver

--- index.js
+++ index.js
@@ -11,22 +11,39 @@ const getDriver = () => {
 }
 const browser = getDriver();
 
+function getElement(elem) {
+    return browser.findElement(webdriver.By.css(elem));
+}
+
 const login = (name, password) => {
-    browser.findElement(webdriver.By.css('#login_field')).sendKeys(name)
-        .then(() => browser.findElement(webdriver.By.css('#password')).sendKeys(password))
-        .then(() => browser.findElement(webdriver.By.css('input[name="commit"]')).click());
+    getElement('#login_field').sendKeys(name)
+        .then(() => getElement('#password').sendKeys(password))
+        .then(() => getElement('input[name="commit"]').click())
 }
 browser.get('https://github.com')
     .then(() => {
-        browser.findElement(webdriver.By.css('a[href="/login"]')).click()
+        getElement('a[href="/login"]').click()
             .then(() => login('UserForTests', 'GitTest_1'));
     })
     .then(() => {
-        browser.findElement(webdriver.By.css('summary[class="HeaderNavlink name mt-1"]')).click()
-        .then(()=>{
-            browser.findElement(webdriver.By.css('strong.css-truncate-target')).getText()
-            .then((result)=>console.log(result));
-        });
-        browser.quit();
+        getElement('summary[class="HeaderNavlink name mt-1"]').click()
+            .then(() => {
+                getElement('strong.css-truncate-target').getText()
+                    .then((result) => console.log(result));
+            });
+        browser.navigate().back()
+            .then(() => browser.navigate().to('https://google.com'))
+            .then(() => browser.navigate().back())
+            .then(() => browser.executeScript('window.open();')
+                .then(() => browser.getAllWindowHandles()
+                    .then((handlers) => browser.switchTo().window(handlers[1]))
+                    .then(() => browser.navigate().to('https://www.w3schools.com/'))
+                    .then(() => getElement('.w3-right').getText()
+                        .then((element) => console.log(element))
+                        .then(() => browser.sleep(4000)
+                            .then(() => browser.close())))
+                    .then(() => browser.getAllWindowHandles()
+                        .then((handlers) => browser.switchTo().window(handlers[0])
+                            .then(() => browser.close())))))
     })
     .catch((err) => { console.log(err) });
